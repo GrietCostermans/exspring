544325e23319a87599532f0ae4c15f223ceac3d9
GrietCostermans <grietje.costermans@bnpparibasfortis.com>
2020-10-07 11:36:51 +0200

    ExB1 nogsteeds met error op Neither BindingResult nor plain target object for bean name 'emailAddress' available as request attribute

--- ExA5/src/main/java/be/abis/exercise/controller/CourseAppController.java
+++ ExA5/src/main/java/be/abis/exercise/controller/CourseAppController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import be.abis.exercise.model.Course;
+import be.abis.exercise.model.Login;
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.TrainingService;
 
@@ -16,25 +17,25 @@ public class CourseAppController {
 	@Autowired
 	TrainingService trainingService; 
 	
-	@GetMapping("/loginForm")
+	@GetMapping("/")
 	public String showLogin(Model model) {
 		//Course course7900 = trainingService.getCourseService().findCourse(7900);
-		//model.addAttribute("course",course7900);
+		model.addAttribute("Login",new Login());
 		return "loginForm";	
 	}
-	
+	/*
 	@PostMapping("/loginForm")
-	public String submitLogin(Model model, Person person) {
+	public String submitLogin(Model model, Login login) {
 		// zoek naam op in person file
 		//Person plogin = findperson();
-		Person personLogged = trainingService.findPerson(person.getEmailAddress(),person.getPassword());
+		Person personLogged = trainingService.findPerson(login.getEmailAddress(),login.getPassword());
 		
 		// als ok dan
 		//    return mainmenu
 		// als nok dan
 		//    return login
 		return "loginForm";
-	}
+	}*/
 	
 	
 	
---
+++ ExA5/src/main/java/be/abis/exercise/model/Login.java
@@ -0,0 +1,41 @@
+package be.abis.exercise.model;
+
+public class Login {
+	private String emailAddress;
+	private String password;
+	
+	public Login(String emailAddress, String password) {
+		super();
+		this.emailAddress = emailAddress;
+		this.password = password;
+	}
+	
+	
+
+	public Login() {
+		super();
+	}
+
+
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+
+}
